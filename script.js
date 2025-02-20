8ccb720b1d618456bb09ccbf5f25be07d6bc2e11
Michael <67581497+MichaelAry@users.noreply.github.com>
2025-02-20 21:12:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var numberLength=4,maxDigit=10;
+function generateNumber(){
+    let tmpArray=[];//to store the generated number during its creation
+    for (;;){
+        if(tmpArray.length<numberLength){
+            let numberDigit=Math.floor(Math.random()*10);
+            if (!tmpArray.includes(numberDigit)){
+                tmpArray[tmpArray.length]=numberDigit;
+            }
+        }
+        else{break;}
+    }
+    return tmpArray;
+}
+let randomNumber = generateNumber();
+console.log(randomNumber);
+
+function validateInput(){
+    var x=document.getElementById("input").value;
+    if (x.length !== numberLength){
+        alert("Пожалуйста, введите ровно 4 различных цифры");
+        return;
+    } 
+    else{
+        for (let i=numberLength-2;i>=0;i--){
+            if((isNaN(parseInt(x[i])))||(x.includes(x[i],i+1))){
+                alert("Пожалуйста, введите ровно 4 различных цифры");
+                return;
+            }
+        }
+    }
+    document.getElementById("demo").innerHTML=x;}
+
+function validateNumber(){}
