95c69c4b0f8590c51146e2d3055a86fefc8cf374
Michael <67581497+MichaelAry@users.noreply.github.com>
2025-02-21 02:25:13 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,63 +1,84 @@
-var numberLength=4,maxDigit=10;
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+let numberLength=4,maxDigit=10,bulls=0,cows=0,attemptsCounter=1,randomNumber=generateNumber();
+/////////////////////////////////////////////////////////////
 function generateNumber(){
     let tmpArray=[];
-    for (;;){
-        if(tmpArray.length<numberLength){
-            let numberDigit=Math.floor(Math.random()*10);
-            if (!tmpArray.includes(numberDigit)){
-                tmpArray[tmpArray.length]=numberDigit;
-            }
+    while(tmpArray.length<numberLength){
+        let numberDigit=Math.floor(Math.random()*10);
+        if(!tmpArray.includes(numberDigit)){
+            tmpArray.push(numberDigit);
         }
-        else{break;}
     }
     return tmpArray;
 }
-let randomNumber = generateNumber();
-console.log(randomNumber);
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
 function validateInput(){
-    var userNumbert=document.getElementById("input").value;
-    if (userNumbert.length!==numberLength){
-        alert("Пожалуйста, введите ровно 4 различных цифры");
-        input.value="";
-        return 0;
-    } 
+    let userNumber=document.getElementById("input").value;
+    if(userNumber.length!==numberLength){
+        alert("введите 4 различных цифры");
+        document.getElementById("input").value="";
+        return "";
+    }
     else{
-        for (let i=numberLength-2;i>=0;i--){
-            if((isNaN(parseInt(userNumbert[i])))||(userNumbert.includes(userNumbert[i],i+1))){
-                alert("Пожалуйста, введите ровно 4 различных цифры");
-                input.value="";
-                return 0;
+        for(let i=numberLength-2;i>=0;i--){
+            if(isNaN(parseInt(userNumber[i]))||userNumber.includes(userNumber[i],i+1)){
+                alert("введите 4 различных цифры");
+                document.getElementById("input").value="";
+                return "";
             }
         }
-       
     }
-    //document.getElementById("demo").innerHTML=userNumber;
-    fillHistoryTable();
-    return userNumbert;
+    return userNumber;
 }
-let userNumber=validateInput();
-console.log(userNumber);
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function fillHistoryTable(){
-    var input = document.getElementById("input").value;
-    var newRow = document.createElement("tr");
-    var newCell = document.createElement("td");
-    newCell.innerHTML = input;
-    newRow.append(newCell);
+/////////////////////////////////////////////////////////////
+function fillHistoryTable(userNumber,bulls,cows){
+    let newRow=document.createElement("tr");
+    let attemptCell=document.createElement("td");
+    attemptCell.innerHTML=attemptsCounter;
+    newRow.append(attemptCell);
+    let numberCell=document.createElement("td");
+    numberCell.innerHTML=userNumber;
+    newRow.append(numberCell);
+    let bullsCell=document.createElement("td");
+    bullsCell.innerHTML=bulls;
+    newRow.append(bullsCell);
+    let cowsCell=document.createElement("td");
+    cowsCell.innerHTML=cows;
+    newRow.append(cowsCell);
     document.getElementById("rows").appendChild(newRow);
-    document.getElementById("input").value = '';
+    document.getElementById("input").value='';
 }
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function valNum_fillTab(){
-    validateInput();
-}
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function defineAnimal(userNumber, randomNumber){
-    for (let i=0;i<numberLength;i++){
-        for(let j=0;j<numberLength;j++){
-
+/////////////////////////////////////////////////////////////
+function defineAnimal(userNumber){
+    bulls=0;
+    cows=0;
+    for(let i=0;i<numberLength;i++){
+        if(userNumber[i]===randomNumber[i].toString()){
+            bulls++;
         }
-    }    
+        else if(randomNumber.includes(parseInt(userNumber[i]))){
+            cows++;
+        }
+    }
 }
+/////////////////////////////////////////////////////////////
+function valNum_fillTab(){
+    let userNumber=validateInput();
+    if(userNumber!==""){
+        defineAnimal(userNumber);
+        fillHistoryTable(userNumber,bulls,cows);
+        attemptsCounter++;
+        if(bulls===numberLength){
+            alert("вы угадали число");
+            resetGame();
+        }
+    }
+}
+/////////////////////////////////////////////////////////////
+function resetGame(){
+    randomNumber=generateNumber();
+    attemptsCounter=1;
+    bulls=0;
+    cows=0;
+    document.getElementById("rows").innerHTML="";
+}
+
